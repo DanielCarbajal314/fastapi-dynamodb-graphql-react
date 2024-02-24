0f7e58ebaa9cc9872384a2b3bcb03cb725df0ecb
Daniel Carbajal <danielcarbajal@Daniels-MacBook-Pro.local>
2024-02-23 20:23:54 -0500

    Fully containarized

--- backend/build/lib/config.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-
-DYNAMODB_URL: str = environ["DYNAMODB_URL"]
-AWS_ACCESS_KEY_ID: str = environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY: str = environ["AWS_SECRET_ACCESS_KEY"]
-AWS_REGION: str = environ["AWS_REGION"]
--- backend/build/lib/database/models/Project.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Project(BaseModel):
-    id: str
-    name: str
